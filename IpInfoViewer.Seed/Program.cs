87501962905fa4f902cf343e7023a971e3a859aa
Peter Trenčanský <peter@trencansky.com>
2022-12-30 13:19:05 +0100

    Seeding of ip address list with location

--- IpInfoViewer.Seed/Program.cs
+++ IpInfoViewer.Seed/Program.cs
@@ -1,10 +1,64 @@
-﻿using IpInfoViewer.Libs.Implementation;
+﻿using System.Globalization;
+using System.Net;
+using System.Text.Json.Serialization;
+using IpInfoViewer.Libs.Implementation;
+using IpInfoViewer.Libs.Models;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using Npgsql.Internal.TypeHandlers.GeometricHandlers;
 
-IpInfoViewerDbRepository repository = new IpInfoViewerDbRepository("Server=127.0.0.1;Port=5432;Database=ipinfoviewerprocesseddb;User Id=postgres;Password=0000;Include Error Detail=true;");
+
+var configBuilder = new ConfigurationBuilder().AddUserSecrets<Program>();
+var config = configBuilder.Build();
+IpInfoViewerDbRepository repository = new IpInfoViewerDbRepository(config["IpInfoViewerProcessedConnectionString"]);
 await repository.SeedTables();
-foreach (var line in File.ReadLines(@"C:\Users\Peter\Downloads\dbip-city-lite-2022-11.csv\dbip-city-lite-2022-11.csv"))
+MFileDbRepository mfileRepo = new MFileDbRepository(config["MFileConnectionString"]);
+var tasks = new List<Task>();
+int seeded = 0;
+int linesPassed = 0;
+//var allHosts = (await mfileRepo.GetAllValidHosts()).ToList();
+await Parallel.ForEachAsync(
+    File.ReadLines(@"C:\Users\Peter\Downloads\dbip-city-lite-2022-11.csv\dbip-city-lite-2022-11.csv"),
+    async (line, token) =>
+    {
+        if (line.Contains("\"")) return;
+        var fields = line.Split(',');
+        var pingsInRange = await mfileRepo.GetHostsInRange(IPAddress.Parse(fields[0]), IPAddress.Parse(fields[1]));
+        //var pingsInRange = allHosts.Where(host => IsGreaterOrEqualThan(host.IpAddr.Item1, IPAddress.Parse(fields[0])) && IsGreaterOrEqualThan(IPAddress.Parse(fields[1]), host.IpAddr.Item1));
+        foreach (var ping in pingsInRange)
+        {
+            var ipInfo = new IpAddressInfo()
+            {
+                City = fields[5],
+                CountryCode = fields[3],
+                IpValue = ping.IpAddr,
+                Latitude = Convert.ToDouble(fields[6], CultureInfo.InvariantCulture),
+                Longitude = Convert.ToDouble(fields[7], CultureInfo.InvariantCulture),
+            };
+            tasks.Add(repository.SaveIpAddressInfo(ipInfo));
+            seeded++;
+            if (seeded % 200 == 0)
+            {
+                Console.WriteLine($"{DateTime.Now} {seeded} seeded");
+            }
+        }
+
+        linesPassed++;
+        if (linesPassed % 2000 == 0)
+        {
+            Console.WriteLine($"{DateTime.Now} {linesPassed} lines passed");
+        }
+    });
+
+await Task.WhenAll(tasks);
+Console.WriteLine("Done.");
+
+static bool IsGreaterOrEqualThan(IPAddress ip1, IPAddress ip2)
 {
-    if (line.Contains("\"")) continue;
-    await repository.SaveIpAddressInfo(line);
+    var bytes1 = ip1.GetAddressBytes();
+    var value1 = bytes1[0] * 256 * 256 * 256 + bytes1[1] * 256 * 256 + bytes1[2] * 256 + bytes1[3];
+    var bytes2 = ip2.GetAddressBytes();
+    var value2 = bytes2[0] * 256 * 256 * 256 + bytes2[1] * 256 * 256 + bytes2[2] * 256 + bytes2[3];
+    return value1 >= value2;
+
 }
